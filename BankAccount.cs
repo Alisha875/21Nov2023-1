12eb1f2d0db44c86c2f748003d02bd5422878d5e
Alisha875 <alishanayak001@gmail.com>
2023-11-21 15:09:46 +0530

    Assignment18

---
+++ BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class BankAccount
+{
+    public int accountNumberSeed = 1234567890;
+    public int accountNumber;
+    public string accountHolderName;
+    public double balance;
+
+    public BankAccount(string accountHolderName, double initialBalance = 0.0)
+    {
+        this.accountHolderName = accountHolderName;
+        this.balance = initialBalance;
+        this.accountNumber = GenerateAccountNumber();
+    }
+
+    public int AccountNumber
+    {
+        get { return accountNumber; }
+    }
+
+    public string AccountHolderName
+    {
+        get { return accountHolderName; }
+        set { accountHolderName = value; }
+    }
+
+    public double Balance
+    {
+        get { return balance; }
+    }
+
+    public void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+        }
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount > 0 && balance >= amount)
+        {
+            balance -= amount;
+        }
+    }
+
+    public int GenerateAccountNumber()
+    {
+        return accountNumberSeed++;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("John Doe", 500.0);
+
+            Console.WriteLine($"Account created with account number: {account.AccountNumber}");
+            Console.WriteLine($"Account balance: {account.Balance}");
+
+            account.Deposit(100.0);
+            Console.WriteLine($"Amount deposited: {100.0}");
+            Console.WriteLine($"Account balance: {account.Balance}");
+
+            account.Withdraw(50.0);
+            Console.WriteLine($"Amount withdrawn: {50.0}");
+            Console.WriteLine($"Account balance: {account.Balance}");
+            Console.ReadKey();
+        }
+    }
+
+}
